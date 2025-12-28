b21ec0b882dcc9ebde184c191643f2e9251d36c1
SunilReddyThuti03 <sunilreddythuti43@gmail.com>
2025-12-28 23:33:18 +0530

    Api development catalog servie

---
+++ catalog-service/src/main/java/com/springbootproject/catalog_service/domain/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.springbootproject.catalog_service.domain;
+
+public class ProductMapper {
+    static Product toProduct(ProductEntity productEntity){
+        return new Product(
+                productEntity.getCode(),
+                productEntity.getName(),
+                productEntity.getDescription(),
+                productEntity.getImageUrl(),
+                productEntity.getPrice()
+        );
+    }
+}
---
+++ catalog-service/src/main/java/com/springbootproject/catalog_service/domain/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springbootproject.catalog_service.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+}
